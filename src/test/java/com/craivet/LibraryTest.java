d384f40ab4b833ab5dae35efc714a006841468a1
Juan Debenedetti <juandebenedetti94@gmail.com>
2022-06-30 22:54:46 -0300

    Added the conventional java directory structure

---
+++ src/test/java/com/craivet/LibraryTest.java
@@ -0,0 +1,17 @@
+package com.craivet;
+
+import org.junit.Test;
+// import static org.junit.Assert.*;
+
+public class LibraryTest {
+
+    @Test
+    public void verifyNoExceptionThrown() {
+        Main.main(new String[]{});
+    }
+
+    /* @Test public void someLibraryMethodReturnsTrue() {
+        Library classUnderTest = new Library();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    } */
+}
